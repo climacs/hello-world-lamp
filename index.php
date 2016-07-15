28c33e712fe03acf11eb79380b50821fb4144e52
Alexey Kapitonov <kapitonoval2012@gmail.com>
2016-07-15 16:37:53 +0300

    edit 1

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-	$mysqli = @new mysqli('localhost','root');
+$mysqli = @new mysqli('localhost','root');
 ?>
 <html>
 <head>
 	<title>Hello world!</title>
 	<style>
-	body {
-		background-color: white;
-		text-align: center;
-		padding: 50px;
-		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-	}
+		body {
+			background-color: white;
+			text-align: center;
+			padding: 50px;
+			font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+		}
 
-	#logo {
-		margin-bottom: 40px;
-	}
+		#logo {
+			margin-bottom: 40px;
+			height:250px;
+		}
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.gif" />
-	<h1><?php echo "Hello world!"; ?></h1>
-	<?php if(!$mysqli) { ?>
-		<h2>Can't connect to local MySQL Server!</h2>
-	<?php } else { ?>
-		<h2>MySQL Server version: <?php echo $mysqli->server_info; ?></h2>
-	<?php } ?>
+<img id="logo" src="logo.gif" />
+<h1><?php echo "Hello world!"; ?></h1>
+<?php if(!$mysqli) { ?>
+	<h2>Can't connect to local MySQL Server!</h2>
+<?php } else { ?>
+	<h2>MySQL Server version: <?php echo $mysqli->server_info; ?></h2>
+<?php } ?>
+<a href="./phpinfo.php">phpinfo</a>
 </body>
 </html>
